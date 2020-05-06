b541f595911a7d3ede6d99ef61cac5df21f6706a
Lachlan Nicholson <lachlan.nicholson@connect.qut.edu.au>
2020-05-06 18:28:26 +1000

    started exposing core c++ classes to python

--- quadricslam.h
+++ quadricslam.h
@@ -18,7 +18,31 @@
 /* -------------------------------------
   NOTES: 
     - cannot have comments after gtsam namespace at eof (i.e, // namespace gtsam)
+    - remove Matrix/Vector dimensions from input/output
+    - remove (void) from input arguments
+    - forward declare return type classes for gtsam objects
+    - show inheritence with virtual + forward declare virtual
+    - for default arguments expose the method twice 
+    - declare returns of vector<gtsamclass> as entire class 
+    - for std::string remove std::
+    - const boost::shared_ptr<X>& -> const X*
+    - const SharedNoiseModel& -> const gtsam::noiseModel::Base* 
+    - size_t -> size_t
+
+  RESTRICTIONS:
+    - cant used boost::shared_ptr<eigenobject>
+
+  QUESTIONS:
+    - how to wrap optional jacobians?
  */
+
+
+
+class Pose3;
+class Point3;
+virtual class NoiseModelFactor;
+class ConstrainedDualQuadric;
+class AlignedBox2;
 
 
 namespace gtsam {
@@ -30,5 +54,50 @@ class TestClass {
   double getx();
   double doWork(double y);
 };
+
+#include <quadricslam/geometry/ConstrainedDualQuadric.h>
+class ConstrainedDualQuadric {
+  ConstrainedDualQuadric();
+  ConstrainedDualQuadric(const Matrix& dQ);
+  ConstrainedDualQuadric(const Pose3& pose, const Vector& radii);
+  ConstrainedDualQuadric(const Rot3& R, const Point3& t, const Vector& r);
+  Pose3 getPose() const;
+  Vector getRadii() const;
+  Point3 centroid() const;
+  Matrix matrix() const;
+  Vector bounds() const;
+  ConstrainedDualQuadric addNoise(const Vector& noiseVector);
+  static ConstrainedDualQuadric Retract(const Vector& v);
+  static Vector LocalCoordinates(const ConstrainedDualQuadric& q);
+  ConstrainedDualQuadric retract(const Vector& v) const;
+  Vector localCoordinates(const ConstrainedDualQuadric& other) const;
+  void print(const string& s) const;
+  void print() const;
+  bool equals(const ConstrainedDualQuadric& other, double tol) const;
+  bool equals(const ConstrainedDualQuadric& other) const;
+};
+
+// #include <quadricslam/geometry/BoundingBoxFactor.h>
+// virtual class BoundingBoxFactor : NoiseModelFactor {
+//   BoundingBoxFactor();
+//   BoundingBoxFactor(const AlignedBox2& measured, const Cal3_S2* calibration, 
+//     const Vector* imageDimensions, const size_t& poseKey, const size_t& quadricKey, 
+//     const gtsam::noiseModel::Base* model);
+//   // Vector evaluateError(const Pose3& pose, const ConstrainedDualQuadric& quadric,
+//   //   boost::optional<Matrix &> H1 = boost::none, boost::optional<Matrix &> H2 = boost::none) const;
+// };
+
+// #include <quadricslam/geometry/AlignedBox2.h>
+// class AlignedBox2 {
+//   AlignedBox2();
+//   AlignedBox2(const double& xmin, const double& ymin, const double& xmax, const double& ymax);
+//   AlignedBox2(const Vector& tlbr);
+//   Vector vector() const;
+//   AlignedBox2 addNoise(const Vector& noiseVector);
+//   void print(const string& s) const;
+//   void print() const;
+//   bool equals(const AlignedBox2& other, double tol) const;
+//   bool equals(const AlignedBox2& other) const;
+// };
 
 }
