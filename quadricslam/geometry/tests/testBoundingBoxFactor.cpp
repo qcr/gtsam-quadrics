7e9c53b671b8bdec51b7b27ac16fe0d8c8d61336
Lachlan Nicholson <lachlan.nicholson@connect.qut.edu.au>
2020-05-21 18:37:59 +1000

    removed expressions and image dimensions from bbf test

--- quadricslam/geometry/tests/testBoundingBoxFactor.cpp
+++ quadricslam/geometry/tests/testBoundingBoxFactor.cpp
@@ -23,13 +23,14 @@
 #include <gtsam/base/Vector.h>
 #include <gtsam/geometry/Pose3.h>
 #include <gtsam/geometry/Rot3.h>
+#include <gtsam/nonlinear/NonlinearFactorGraph.h>
 
 using namespace std;
 using namespace gtsam;
 
+static NonlinearFactorGraph graph;
 static AlignedBox2 measured(15.2, 18.5, 120.5, 230.2);
 static boost::shared_ptr<Cal3_S2> calibration(new Cal3_S2(525.0, 525.0, 0.0, 320.0, 240.0));
-static Vector2 imageDimensions(320.0, 240.0);
 static Key poseKey(Symbol('x', 1));
 static Key quadricKey(Symbol('q', 1));
 static boost::shared_ptr<noiseModel::Diagonal> model = noiseModel::Diagonal::Sigmas(Vector4(0.2,0.2,0.2,0.2));
@@ -41,28 +42,15 @@ TEST(BoundingBoxFactor, Constructors) {
 }
 
 TEST(BoundingBoxFactor, Error) {
-  BoundingBoxFactor bbf(measured, calibration, imageDimensions, poseKey, quadricKey, model);
+  BoundingBoxFactor bbf(measured, calibration, poseKey, quadricKey, model);
   bbf.evaluateError(cameraPose, quadric);
 }
 
-TEST(BoundingBoxFactor, ExpressionError) {
-  BoundingBoxFactor bbf(measured, calibration, imageDimensions, poseKey, quadricKey, model);
-
-  Expression<Pose3> cameraPose_('x',1);
-  Expression<ConstrainedDualQuadric> quadric_('q',1);
-  Values values;
-  values.insert(symbol('x',1), cameraPose);
-  values.insert(symbol('q',1), quadric);
-  std::vector<Matrix> gradients;
-  Eigen::Matrix<double, 4,6> db_dx;
-  Eigen::Matrix<double, 4,9> db_dq;
-  gradients.push_back(db_dq);
-  gradients.push_back(db_dx);
-
-  Expression<AlignedBox2> bbfExpression = bbf.expression(cameraPose_, quadric_);
-  bbfExpression.value(values, gradients);
-  db_dq = gradients[0];
-  db_dx = gradients[1];
+TEST(BoundingBoxFactor, AddRemoveFromGraph) {
+  BoundingBoxFactor bbf(measured, calibration, poseKey, quadricKey, model);
+  graph.add(bbf);
+  auto nlf = graph.at(0);
+  BoundingBoxFactor bbf2 = *boost::dynamic_pointer_cast<BoundingBoxFactor>(graph.at(0)).get();
 }
 
 /* ************************************************************************* */
