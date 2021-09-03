800ec7b6bb3c4c7d16e548519f70df6489dbfd01
Ben Talbot <b.talbot@qut.edu.au>
2021-09-03 13:32:34 +1000

    Explicitly add <boost/function.hpp> imports

--- gtsam_quadrics/geometry/BoundingBoxFactor.cpp
+++ gtsam_quadrics/geometry/BoundingBoxFactor.cpp
@@ -1,6 +1,7 @@
 /* ----------------------------------------------------------------------------
 
- * QuadricSLAM Copyright 2020, ARC Centre of Excellence for Robotic Vision, Queensland University of Technology (QUT)
+ * QuadricSLAM Copyright 2020, ARC Centre of Excellence for Robotic Vision,
+ Queensland University of Technology (QUT)
  * Brisbane, QLD 4000
  * All Rights Reserved
  * Authors: Lachlan Nicholson, et al. (see THANKS for the full author list)
@@ -15,11 +16,12 @@
  * @brief factor between Pose3 and ConstrainedDualQuadric
  */
 
+#include <gtsam/base/numericalDerivative.h>
+#include <gtsam_quadrics/base/QuadricProjectionException.h>
 #include <gtsam_quadrics/geometry/BoundingBoxFactor.h>
 #include <gtsam_quadrics/geometry/QuadricCamera.h>
-#include <gtsam_quadrics/base/QuadricProjectionException.h>
 
-#include <gtsam/base/numericalDerivative.h>
+#include <boost/function.hpp>
 
 #define NUMERICAL_DERIVATIVE false
 
@@ -28,23 +30,26 @@ using namespace std;
 namespace gtsam {
 
 /* ************************************************************************* */
-Vector BoundingBoxFactor::evaluateError(const Pose3& pose, const ConstrainedDualQuadric& quadric,
-  boost::optional<Matrix &> H1, boost::optional<Matrix &> H2) const {
-
+Vector BoundingBoxFactor::evaluateError(const Pose3& pose,
+                                        const ConstrainedDualQuadric& quadric,
+                                        boost::optional<Matrix&> H1,
+                                        boost::optional<Matrix&> H2) const {
   try {
-
     // check pose-quadric pair
     if (quadric.isBehind(pose)) {
       throw QuadricProjectionException("Quadric is behind camera");
-    } if (quadric.contains(pose)) {
+    }
+    if (quadric.contains(pose)) {
       throw QuadricProjectionException("Camera is inside quadric");
     }
 
-    // project quadric taking into account partial derivatives 
-    Eigen::Matrix<double, 9,6> dC_dx; Eigen::Matrix<double, 9,9> dC_dq;
+    // project quadric taking into account partial derivatives
+    Eigen::Matrix<double, 9, 6> dC_dx;
+    Eigen::Matrix<double, 9, 9> dC_dq;
     DualConic dualConic;
     if (!NUMERICAL_DERIVATIVE) {
-      dualConic = QuadricCamera::project(quadric, pose, calibration_, H2?&dC_dq:0, H1?&dC_dx:0);
+      dualConic = QuadricCamera::project(quadric, pose, calibration_,
+                                         H2 ? &dC_dq : 0, H1 ? &dC_dx : 0);
     } else {
       dualConic = QuadricCamera::project(quadric, pose, calibration_);
     }
@@ -55,89 +60,90 @@ Vector BoundingBoxFactor::evaluateError(const Pose3& pose, const ConstrainedDual
     }
 
     // calculate conic bounds with derivatives
-    bool computeJacobians = bool(H1||H2) && !NUMERICAL_DERIVATIVE; 
-    Eigen::Matrix<double, 4,9> db_dC;
+    bool computeJacobians = bool(H1 || H2) && !NUMERICAL_DERIVATIVE;
+    Eigen::Matrix<double, 4, 9> db_dC;
     AlignedBox2 predictedBounds;
     if (measurementModel_ == STANDARD) {
-      predictedBounds = dualConic.bounds(computeJacobians?&db_dC:0);     
+      predictedBounds = dualConic.bounds(computeJacobians ? &db_dC : 0);
     } else if (measurementModel_ == TRUNCATED) {
-
       try {
-        predictedBounds = dualConic.smartBounds(calibration_, computeJacobians?&db_dC:0); 
+        predictedBounds =
+            dualConic.smartBounds(calibration_, computeJacobians ? &db_dC : 0);
       } catch (std::runtime_error& e) {
         throw QuadricProjectionException("smartbounds failed");
       }
-
-      
     }
 
-    // evaluate error 
+    // evaluate error
     Vector4 error = predictedBounds.vector() - measured_.vector();
 
-
     if (NUMERICAL_DERIVATIVE) {
-      boost::function<Vector(const Pose3&, const ConstrainedDualQuadric&)> funPtr(boost::bind(&BoundingBoxFactor::evaluateError, this, _1, _2, boost::none, boost::none));
+      boost::function<Vector(const Pose3&, const ConstrainedDualQuadric&)>
+          funPtr(boost::bind(&BoundingBoxFactor::evaluateError, this, _1, _2,
+                             boost::none, boost::none));
       if (H1) {
-        Eigen::Matrix<double, 4,6> db_dx_ = numericalDerivative21(funPtr, pose, quadric, 1e-6);
+        Eigen::Matrix<double, 4, 6> db_dx_ =
+            numericalDerivative21(funPtr, pose, quadric, 1e-6);
         *H1 = db_dx_;
-      } if (H2) {
-        Eigen::Matrix<double, 4,9> db_dq_ = numericalDerivative22(funPtr, pose, quadric, 1e-6);
+      }
+      if (H2) {
+        Eigen::Matrix<double, 4, 9> db_dq_ =
+            numericalDerivative22(funPtr, pose, quadric, 1e-6);
         *H2 = db_dq_;
       }
     } else {
-
       // calculate derivative of error wrt pose
       if (H1) {
-
-        // combine partial derivatives 
+        // combine partial derivatives
         *H1 = db_dC * dC_dx;
-      } 
-      
-      // calculate derivative of error wrt quadric
-      if (H2) {
-
-        // combine partial derivatives 
-        *H2 = db_dC * dC_dq; 
       }
 
+      // calculate derivative of error wrt quadric
+      if (H2) {
+        // combine partial derivatives
+        *H2 = db_dC * dC_dq;
+      }
     }
 
     return error;
 
     // check for nans
-    if (error.array().isInf().any() || error.array().isNaN().any()
-      || (H1 && (H1->array().isInf().any() || H1->array().isNaN().any()))
-      || (H2 && (H2->array().isInf().any() || H2->array().isNaN().any()))) {
+    if (error.array().isInf().any() || error.array().isNaN().any() ||
+        (H1 && (H1->array().isInf().any() || H1->array().isNaN().any())) ||
+        (H2 && (H2->array().isInf().any() || H2->array().isNaN().any()))) {
       throw std::runtime_error("nan/inf error in bbf");
     }
 
+    // handle projection failures
+  } catch (QuadricProjectionException& e) {
+    // std::cout << "  Landmark " << symbolIndex(this->objectKey()) << "
+    // received: " << e.what() << std::endl;
 
-
-
-  // handle projection failures
-  } catch(QuadricProjectionException& e) {
-    // std::cout << "  Landmark " << symbolIndex(this->objectKey()) << " received: " << e.what() << std::endl;
-    
     // if error cannot be calculated
     // set error vector and jacobians to zero
-    Vector4 error = Vector4::Ones()*1000.0;
-    if (H1) {*H1 = Matrix::Zero(4,6);}
-    if (H2) {*H2 = Matrix::Zero(4,9);}
+    Vector4 error = Vector4::Ones() * 1000.0;
+    if (H1) {
+      *H1 = Matrix::Zero(4, 6);
+    }
+    if (H2) {
+      *H2 = Matrix::Zero(4, 9);
+    }
 
     return error;
-
   }
 }
 
 /* ************************************************************************* */
-Matrix BoundingBoxFactor::evaluateH1(const Pose3& pose, const ConstrainedDualQuadric& quadric) const {
+Matrix BoundingBoxFactor::evaluateH1(
+    const Pose3& pose, const ConstrainedDualQuadric& quadric) const {
   Matrix H1;
   this->evaluateError(pose, quadric, H1, boost::none);
   return H1;
 }
 
 /* ************************************************************************* */
-Matrix BoundingBoxFactor::evaluateH2(const Pose3& pose, const ConstrainedDualQuadric& quadric) const {
+Matrix BoundingBoxFactor::evaluateH2(
+    const Pose3& pose, const ConstrainedDualQuadric& quadric) const {
   Matrix H2;
   this->evaluateError(pose, quadric, boost::none, H2);
   return H2;
@@ -146,31 +152,38 @@ Matrix BoundingBoxFactor::evaluateH2(const Pose3& pose, const ConstrainedDualQua
 /* ************************************************************************* */
 Matrix BoundingBoxFactor::evaluateH1(const Values& x) const {
   const Pose3 pose = x.at<Pose3>(this->poseKey());
-  const ConstrainedDualQuadric quadric = x.at<ConstrainedDualQuadric>(this->objectKey());
+  const ConstrainedDualQuadric quadric =
+      x.at<ConstrainedDualQuadric>(this->objectKey());
   return this->evaluateH1(pose, quadric);
 }
 
 /* ************************************************************************* */
 Matrix BoundingBoxFactor::evaluateH2(const Values& x) const {
   const Pose3 pose = x.at<Pose3>(this->poseKey());
-  const ConstrainedDualQuadric quadric = x.at<ConstrainedDualQuadric>(this->objectKey());
+  const ConstrainedDualQuadric quadric =
+      x.at<ConstrainedDualQuadric>(this->objectKey());
   return this->evaluateH2(pose, quadric);
 }
 
 /* ************************************************************************* */
-void BoundingBoxFactor::print(const std::string& s, const KeyFormatter& keyFormatter) const {
-  cout << s << "BoundingBoxFactor(" << keyFormatter(key1()) << "," << keyFormatter(key2()) << ")" << endl;
+void BoundingBoxFactor::print(const std::string& s,
+                              const KeyFormatter& keyFormatter) const {
+  cout << s << "BoundingBoxFactor(" << keyFormatter(key1()) << ","
+       << keyFormatter(key2()) << ")" << endl;
   measured_.print("    Measured: ");
-  cout << "    NoiseModel: "; noiseModel()->print(); cout << endl;
+  cout << "    NoiseModel: ";
+  noiseModel()->print();
+  cout << endl;
 }
 
 /* ************************************************************************* */
-bool BoundingBoxFactor::equals(const BoundingBoxFactor& other, double tol) const {
-  bool equal = measured_.equals(other.measured_, tol)
-    && calibration_->equals(*other.calibration_, tol)
-    && noiseModel()->equals(*other.noiseModel(), tol)
-    && key1() == other.key1() && key2() == other.key2();
+bool BoundingBoxFactor::equals(const BoundingBoxFactor& other,
+                               double tol) const {
+  bool equal = measured_.equals(other.measured_, tol) &&
+               calibration_->equals(*other.calibration_, tol) &&
+               noiseModel()->equals(*other.noiseModel(), tol) &&
+               key1() == other.key1() && key2() == other.key2();
   return equal;
 }
 
-} // namespace gtsam
+}  // namespace gtsam
--- gtsam_quadrics/geometry/DualConic.cpp
+++ gtsam_quadrics/geometry/DualConic.cpp
@@ -1,6 +1,7 @@
 /* ----------------------------------------------------------------------------
 
- * QuadricSLAM Copyright 2020, ARC Centre of Excellence for Robotic Vision, Queensland University of Technology (QUT)
+ * QuadricSLAM Copyright 2020, ARC Centre of Excellence for Robotic Vision,
+ Queensland University of Technology (QUT)
  * Brisbane, QLD 4000
  * All Rights Reserved
  * Authors: Lachlan Nicholson, et al. (see THANKS for the full author list)
@@ -15,16 +16,16 @@
  * @brief a dual conic
  */
 
+#include <gtsam/base/numericalDerivative.h>
+#include <gtsam_quadrics/base/QuadricProjectionException.h>
+#include <gtsam_quadrics/base/Utilities.h>
 #include <gtsam_quadrics/geometry/DualConic.h>
 #include <gtsam_quadrics/geometry/QuadricCamera.h>
-#include <gtsam_quadrics/base/Utilities.h>
-#include <gtsam_quadrics/base/QuadricProjectionException.h>
 
-#include <gtsam/base/numericalDerivative.h>
-
-#include <iostream>
-#include <iomanip>
+#include <boost/function.hpp>
 #include <cmath>
+#include <iomanip>
+#include <iostream>
 
 using namespace std;
 
@@ -32,50 +33,61 @@ namespace gtsam {
 
 /* ************************************************************************* */
 DualConic::DualConic() {
-  dC_ = (Matrix33() << 1.0, 0.0, 0.0, 
-                        0.0, 1.0, 0.0,
-                        0.0, 0.0, -1.0).finished();
+  dC_ = (Matrix33() << 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, -1.0).finished();
 }
 
 /* ************************************************************************* */
 DualConic::DualConic(const Pose2& pose, const Vector2& radii) {
   Matrix33 Z = pose.matrix();
-  Matrix33 Cc = (Vector3() << (radii).array().pow(2), -1.0).finished().asDiagonal();
+  Matrix33 Cc =
+      (Vector3() << (radii).array().pow(2), -1.0).finished().asDiagonal();
   Matrix33 dC = Z * Cc * Z.transpose();
   dC_ = dC;
 }
 
 /* ************************************************************************* */
 DualConic DualConic::normalize(void) const {
-  return DualConic(dC_/dC_(2,2));
+  return DualConic(dC_ / dC_(2, 2));
 }
 
 /* ************************************************************************* */
 /// TODO: assert conic is closed (eccentricity)
 /// assert bounds are real-valued
 /// normalize conic
-AlignedBox2 DualConic::bounds(OptionalJacobian<4,9> H) const {
-  double xmin = (dC_(0,2) + sqrt(dC_(0,2)*dC_(0,2)-dC_(2,2)*dC_(0,0))) / dC_(2,2);
-  double xmax = (dC_(0,2) - sqrt(dC_(0,2)*dC_(0,2)-dC_(2,2)*dC_(0,0))) / dC_(2,2);
-  double ymin = (dC_(1,2) + sqrt(dC_(1,2)*dC_(1,2)-dC_(2,2)*dC_(1,1))) / dC_(2,2);
-  double ymax = (dC_(1,2) - sqrt(dC_(1,2)*dC_(1,2)-dC_(2,2)*dC_(1,1))) / dC_(2,2);
+AlignedBox2 DualConic::bounds(OptionalJacobian<4, 9> H) const {
+  double xmin =
+      (dC_(0, 2) + sqrt(dC_(0, 2) * dC_(0, 2) - dC_(2, 2) * dC_(0, 0))) /
+      dC_(2, 2);
+  double xmax =
+      (dC_(0, 2) - sqrt(dC_(0, 2) * dC_(0, 2) - dC_(2, 2) * dC_(0, 0))) /
+      dC_(2, 2);
+  double ymin =
+      (dC_(1, 2) + sqrt(dC_(1, 2) * dC_(1, 2) - dC_(2, 2) * dC_(1, 1))) /
+      dC_(2, 2);
+  double ymax =
+      (dC_(1, 2) - sqrt(dC_(1, 2) * dC_(1, 2) - dC_(2, 2) * dC_(1, 1))) /
+      dC_(2, 2);
 
   if (H) {
-    Eigen::Matrix<double, 4,9> db_dC = Matrix::Zero(4,9);
-    double f = sqrt(dC_(0,2)*dC_(0,2)-dC_(0,0)*dC_(2,2));
-    double g = sqrt(dC_(1,2)*dC_(1,2)-dC_(1,1)*dC_(2,2));
-    db_dC(0,0) = 1.0/f*(-1.0/2.0);
-    db_dC(0,6) = (dC_(0,2)*1.0/f+1.0)/dC_(2,2);
-    db_dC(0,8) = -1.0/(dC_(2,2)*dC_(2,2))*(dC_(0,2)+f)-(dC_(0,0)*1.0/f*(1.0/2.0))/dC_(2,2);
-    db_dC(1,4) = 1.0/g*(-1.0/2.0);
-    db_dC(1,7) = (dC_(1,2)*1.0/g+1.0)/dC_(2,2);
-    db_dC(1,8) = -1.0/(dC_(2,2)*dC_(2,2))*(dC_(1,2)+g)-(dC_(1,1)*1.0/g*(1.0/2.0))/dC_(2,2);
-    db_dC(2,0) = 1.0/f*(1.0/2.0);
-    db_dC(2,6) = -(dC_(0,2)*1.0/f-1.0)/dC_(2,2);
-    db_dC(2,8) = -1.0/(dC_(2,2)*dC_(2,2))*(dC_(0,2)-f)+(dC_(0,0)*1.0/f*(1.0/2.0))/dC_(2,2);
-    db_dC(3,4) = 1.0/g*(1.0/2.0);
-    db_dC(3,7) = -(dC_(1,2)*1.0/g-1.0)/dC_(2,2);
-    db_dC(3,8) = -1.0/(dC_(2,2)*dC_(2,2))*(dC_(1,2)-g)+(dC_(1,1)*1.0/g*(1.0/2.0))/dC_(2,2);
+    Eigen::Matrix<double, 4, 9> db_dC = Matrix::Zero(4, 9);
+    double f = sqrt(dC_(0, 2) * dC_(0, 2) - dC_(0, 0) * dC_(2, 2));
+    double g = sqrt(dC_(1, 2) * dC_(1, 2) - dC_(1, 1) * dC_(2, 2));
+    db_dC(0, 0) = 1.0 / f * (-1.0 / 2.0);
+    db_dC(0, 6) = (dC_(0, 2) * 1.0 / f + 1.0) / dC_(2, 2);
+    db_dC(0, 8) = -1.0 / (dC_(2, 2) * dC_(2, 2)) * (dC_(0, 2) + f) -
+                  (dC_(0, 0) * 1.0 / f * (1.0 / 2.0)) / dC_(2, 2);
+    db_dC(1, 4) = 1.0 / g * (-1.0 / 2.0);
+    db_dC(1, 7) = (dC_(1, 2) * 1.0 / g + 1.0) / dC_(2, 2);
+    db_dC(1, 8) = -1.0 / (dC_(2, 2) * dC_(2, 2)) * (dC_(1, 2) + g) -
+                  (dC_(1, 1) * 1.0 / g * (1.0 / 2.0)) / dC_(2, 2);
+    db_dC(2, 0) = 1.0 / f * (1.0 / 2.0);
+    db_dC(2, 6) = -(dC_(0, 2) * 1.0 / f - 1.0) / dC_(2, 2);
+    db_dC(2, 8) = -1.0 / (dC_(2, 2) * dC_(2, 2)) * (dC_(0, 2) - f) +
+                  (dC_(0, 0) * 1.0 / f * (1.0 / 2.0)) / dC_(2, 2);
+    db_dC(3, 4) = 1.0 / g * (1.0 / 2.0);
+    db_dC(3, 7) = -(dC_(1, 2) * 1.0 / g - 1.0) / dC_(2, 2);
+    db_dC(3, 8) = -1.0 / (dC_(2, 2) * dC_(2, 2)) * (dC_(1, 2) - g) +
+                  (dC_(1, 1) * 1.0 / g * (1.0 / 2.0)) / dC_(2, 2);
     *H = db_dC;
   }
 
@@ -83,8 +95,9 @@ AlignedBox2 DualConic::bounds(OptionalJacobian<4,9> H) const {
 }
 
 /* ************************************************************************* */
-AlignedBox2 DualConic::smartBounds(const boost::shared_ptr<Cal3_S2>& calibration, OptionalJacobian<4,9> H) const {
-
+AlignedBox2 DualConic::smartBounds(
+    const boost::shared_ptr<Cal3_S2>& calibration,
+    OptionalJacobian<4, 9> H) const {
   // calculate image dimensions from calibration
   // double imageWidth = calibration->px() * 2.0;
   // double imageHeight = calibration->py() * 2.0;
@@ -92,65 +105,69 @@ AlignedBox2 DualConic::smartBounds(const boost::shared_ptr<Cal3_S2>& calibration
   double imageHeight = 480.0;
   AlignedBox2 imageBounds(0.0, 0.0, imageWidth, imageHeight);
 
-  // if quadric is fully visible, use the faster simple bounds 
-  Eigen::Matrix<double, 4,9> simpleJacobian;
-  AlignedBox2 simpleBounds = this->bounds(H?&simpleJacobian:0);
+  // if quadric is fully visible, use the faster simple bounds
+  Eigen::Matrix<double, 4, 9> simpleJacobian;
+  AlignedBox2 simpleBounds = this->bounds(H ? &simpleJacobian : 0);
   if (imageBounds.contains(simpleBounds)) {
-    if (H) {*H = simpleJacobian;}
+    if (H) {
+      *H = simpleJacobian;
+    }
     return simpleBounds;
   }
 
-  // ensure quadric is at least partially visible 
-  // NOTE: this will not work because bounds can be inside whilst conic is completely outside imagebounds
-  // if (!imageBounds.contains(simpleBounds) && !imageBounds.intersects(simpleBounds)) {
-  //   throw QuadricProjectionException("SimpleBounds outside ImageBounds, implies quadric not visible");
+  // ensure quadric is at least partially visible
+  // NOTE: this will not work because bounds can be inside whilst conic is
+  // completely outside imagebounds if (!imageBounds.contains(simpleBounds) &&
+  // !imageBounds.intersects(simpleBounds)) {
+  //   throw QuadricProjectionException("SimpleBounds outside ImageBounds,
+  //   implies quadric not visible");
   // }
 
-  // cast dual conic to long double before inverting 
+  // cast dual conic to long double before inverting
   /// NOTE: this helps keep polynomials from having -ve desc
-  Eigen::Matrix<long double, 3,3> dualConic = dC_.cast<long double>();
-  dualConic = dualConic/dualConic(2,2);
+  Eigen::Matrix<long double, 3, 3> dualConic = dC_.cast<long double>();
+  dualConic = dualConic / dualConic(2, 2);
 
-  // calculate point conic 
-  Eigen::Matrix<long double, 3,3> C = dualConic.inverse();
+  // calculate point conic
+  Eigen::Matrix<long double, 3, 3> C = dualConic.inverse();
 
-  // normalize conic so polynomials behave 
-  C = C/C(2,2);
-  
-  std::vector<Point2> points; 
+  // normalize conic so polynomials behave
+  C = C / C(2, 2);
+
+  std::vector<Point2> points;
 
   try {
     // solve intersection of dC/dx and conic C (solving y values first)
     Vector2 ys = gtsam::utils::solvePolynomial(
-      C(1,1)-pow(C(1,0)*2,2)/(4*C(0,0)),
-      C(2,1)*2-(C(1,0)*2*C(2,0)*2)/(2*C(0,0)),
-      C(2,2)-pow(C(2,0)*2,2)/(4*C(0,0))
-    );
-    Point2 p1( (-C(1,0)*2*ys[0]-C(2,0)*2)/(2*C(0,0)), ys[0] );
-    Point2 p3( (-C(1,0)*2*ys[1]-C(2,0)*2)/(2*C(0,0)), ys[1] );
+        C(1, 1) - pow(C(1, 0) * 2, 2) / (4 * C(0, 0)),
+        C(2, 1) * 2 - (C(1, 0) * 2 * C(2, 0) * 2) / (2 * C(0, 0)),
+        C(2, 2) - pow(C(2, 0) * 2, 2) / (4 * C(0, 0)));
+    Point2 p1((-C(1, 0) * 2 * ys[0] - C(2, 0) * 2) / (2 * C(0, 0)), ys[0]);
+    Point2 p3((-C(1, 0) * 2 * ys[1] - C(2, 0) * 2) / (2 * C(0, 0)), ys[1]);
 
     // solve intersection of dC/dx and conic C (solving x values first)
     // Vector2 xs = gtsam::utils::solvePolynomial(
     //   4*pow(C(0,0),2)*C(1,1)/pow(C(1,0)*2,2) - C(0,0),
     //   4*C(0,0)*C(1,1)*C(2,0)*2/pow(C(1,0)*2,2) - 2*C(0,0)*C(2,1)*2/C(1,0)*2,
-    //   C(1,1)*pow(C(2,0)*2,2)/pow(C(1,0)*2,2) - C(2,0)*2*C(2,1)*2/C(1,0)*2 + C(2,2)
+    //   C(1,1)*pow(C(2,0)*2,2)/pow(C(1,0)*2,2) - C(2,0)*2*C(2,1)*2/C(1,0)*2 +
+    //   C(2,2)
     // );
 
     // solve intersection of dC/dy and conic C (solving y values first)
     // Vector2 ys = gtsam::utils::solvePolynomial(
     //   4*C(0,0)*pow(C(1,1),2)/pow(C(1,0)*2,2) - C(1,1),
-    //   4*C(0,0)*C(1,1)*C(2,1)*2/pow(C(1,0)*2,2) - (2*C(1,1)*C(2,0)*2/C(1,0)*2),
-    //   C(0,0)*pow(C(2,1)*2,2)/pow(C(1,0)*2,2) - (C(2,0)*2*C(2,1)*2/C(1,0)*2) + C(2,2)
+    //   4*C(0,0)*C(1,1)*C(2,1)*2/pow(C(1,0)*2,2) -
+    //   (2*C(1,1)*C(2,0)*2/C(1,0)*2), C(0,0)*pow(C(2,1)*2,2)/pow(C(1,0)*2,2) -
+    //   (C(2,0)*2*C(2,1)*2/C(1,0)*2) + C(2,2)
     // );
 
     // solve intersection of dC/dy and conic C (solving x values first)
     Vector2 xs = gtsam::utils::solvePolynomial(
-      C(0,0) - pow(C(1,0)*2,2)/(4*C(1,1)),
-      C(2,0)*2 - (C(1,0)*2*C(2,1)*2)/(2*C(1,1)),
-      C(2,2) - pow(C(2,1)*2,2)/(4*C(1,1))
-    );
-    Point2 p0( xs[0], (-C(1,0)*2*xs[0]-C(2,1)*2)/(2*C(1,1)) );
-    Point2 p2( xs[1], (-C(1,0)*2*xs[1]-C(2,1)*2)/(2*C(1,1)) );
+        C(0, 0) - pow(C(1, 0) * 2, 2) / (4 * C(1, 1)),
+        C(2, 0) * 2 - (C(1, 0) * 2 * C(2, 1) * 2) / (2 * C(1, 1)),
+        C(2, 2) - pow(C(2, 1) * 2, 2) / (4 * C(1, 1)));
+    Point2 p0(xs[0], (-C(1, 0) * 2 * xs[0] - C(2, 1) * 2) / (2 * C(1, 1)));
+    Point2 p2(xs[1], (-C(1, 0) * 2 * xs[1] - C(2, 1) * 2) / (2 * C(1, 1)));
 
     // append extrema to set of points
     points.push_back(p0);
@@ -161,44 +178,55 @@ AlignedBox2 DualConic::smartBounds(const boost::shared_ptr<Cal3_S2>& calibration
     throw e;
   }
 
-
-  // intersection of conic and line at X = 0 
+  // intersection of conic and line at X = 0
   try {
     Vector2 ys = gtsam::utils::getConicPointsAtX(C, 0.0);
     points.push_back(Point2(0.0, ys[0]));
     points.push_back(Point2(0.0, ys[1]));
-  } catch(std::runtime_error& e) { }
+  } catch (std::runtime_error& e) {
+  }
 
   // intersection of conic and line at X = width
   try {
     Vector2 ys = gtsam::utils::getConicPointsAtX(C, imageWidth);
     points.push_back(Point2(imageWidth, ys[0]));
     points.push_back(Point2(imageWidth, ys[1]));
-  } catch(std::runtime_error& e) { }
+  } catch (std::runtime_error& e) {
+  }
 
-  // intersection of conic and line at Y = 0 
+  // intersection of conic and line at Y = 0
   try {
     Vector2 xs = gtsam::utils::getConicPointsAtY(C, 0.0);
     points.push_back(Point2(xs[0], 0.0));
     points.push_back(Point2(xs[1], 0.0));
-  } catch(std::runtime_error& e) { }
+  } catch (std::runtime_error& e) {
+  }
 
   // intersection of conic and line at Y = height
   try {
     Vector2 xs = gtsam::utils::getConicPointsAtY(C, imageHeight);
     points.push_back(Point2(xs[0], imageHeight));
     points.push_back(Point2(xs[1], imageHeight));
-  } catch(std::runtime_error& e) { }
+  } catch (std::runtime_error& e) {
+  }
 
-  // push back any captured image boundaries 
+  // push back any captured image boundaries
   Point2 i1(0.0, 0.0);
   Point2 i2(0.0, imageHeight);
   Point2 i3(imageWidth, 0.0);
   Point2 i4(imageWidth, imageHeight);
-  if (this->contains(i1)) { points.push_back(i1);}
-  if (this->contains(i2)) { points.push_back(i2);}
-  if (this->contains(i3)) { points.push_back(i3);}
-  if (this->contains(i4)) { points.push_back(i4);}
+  if (this->contains(i1)) {
+    points.push_back(i1);
+  }
+  if (this->contains(i2)) {
+    points.push_back(i2);
+  }
+  if (this->contains(i3)) {
+    points.push_back(i3);
+  }
+  if (this->contains(i4)) {
+    points.push_back(i4);
+  }
 
   // only accept non-imaginary points within image boundaries
   /// NOTE: it's important that contains includes points on the boundary
@@ -211,33 +239,42 @@ AlignedBox2 DualConic::smartBounds(const boost::shared_ptr<Cal3_S2>& calibration
   }
 
   if (validPoints.size() < 1) {
-    throw std::runtime_error("no valid conic points inside image dimensions, implies quadric not visible");
+    throw std::runtime_error(
+        "no valid conic points inside image dimensions, implies quadric not "
+        "visible");
     // simpleBounds.print("Failed SimpleBounds:");
-    // throw QuadricProjectionException("No valid conic points inside image dimensions, implies quadric not visible");
+    // throw QuadricProjectionException("No valid conic points inside image
+    // dimensions, implies quadric not visible");
   }
-  auto minMaxX = std::minmax_element(validPoints.begin(), validPoints.end(), 
-    [] (const Point2& lhs, const Point2& rhs) {return lhs.x() < rhs.x();});
-  auto minMaxY = std::minmax_element(validPoints.begin(), validPoints.end(), 
-    [] (const Point2& lhs, const Point2& rhs) {return lhs.y() < rhs.y();});
+  auto minMaxX = std::minmax_element(
+      validPoints.begin(), validPoints.end(),
+      [](const Point2& lhs, const Point2& rhs) { return lhs.x() < rhs.x(); });
+  auto minMaxY = std::minmax_element(
+      validPoints.begin(), validPoints.end(),
+      [](const Point2& lhs, const Point2& rhs) { return lhs.y() < rhs.y(); });
 
   // take the max/min of remaining points
-  AlignedBox2 smartBounds(minMaxX.first->x(), minMaxY.first->y(), minMaxX.second->x(), minMaxY.second->y());
+  AlignedBox2 smartBounds(minMaxX.first->x(), minMaxY.first->y(),
+                          minMaxX.second->x(), minMaxY.second->y());
 
   // calculate jacobians
   if (H) {
-
     // we want to derive wrt vector output and matrix input
-    auto bounds_funptr = [&](const Matrix33& conic_matrix) -> Vector { 
-      return DualConic(conic_matrix).smartBounds(calibration, boost::none).vector();
-    }; 
+    auto bounds_funptr = [&](const Matrix33& conic_matrix) -> Vector {
+      return DualConic(conic_matrix)
+          .smartBounds(calibration, boost::none)
+          .vector();
+    };
 
-    // cast to boost::function for numericalDerivative 
-    auto boost_funptr(static_cast<boost::function<Vector(const Matrix33&)>>(bounds_funptr)); 
+    // cast to boost::function for numericalDerivative
+    auto boost_funptr(
+        static_cast<boost::function<Vector(const Matrix33&)>>(bounds_funptr));
 
-    // calculate derivative of conic_matrix wrt quadric vector 
-    Eigen::Matrix<double, 4,9> db_dC = numericalDerivative11(boost_funptr, this->matrix(), 1e-6); 
+    // calculate derivative of conic_matrix wrt quadric vector
+    Eigen::Matrix<double, 4, 9> db_dC =
+        numericalDerivative11(boost_funptr, this->matrix(), 1e-6);
 
-    // set jacobian to numerical derivative 
+    // set jacobian to numerical derivative
     *H = db_dC;
   }
   return smartBounds;
@@ -246,7 +283,8 @@ AlignedBox2 DualConic::smartBounds(const boost::shared_ptr<Cal3_S2>& calibration
 /* ************************************************************************* */
 bool DualConic::isDegenerate(void) const {
   Matrix33 C = dC_.inverse();
-  if (C.determinant() == 0.0 || C.array().isInf().any() || C.array().isNaN().any()) {
+  if (C.determinant() == 0.0 || C.array().isInf().any() ||
+      C.array().isNaN().any()) {
     return true;
   }
   return false;
@@ -255,10 +293,10 @@ bool DualConic::isDegenerate(void) const {
 /* ************************************************************************* */
 bool DualConic::isEllipse(void) const {
   Matrix33 C = dC_.inverse();
-  C = C/C(2,2);
+  C = C / C(2, 2);
   bool isDegenerate = C.determinant() == 0.0;
   if (!isDegenerate) {
-    Matrix22 A33 = C.block(0,0,2,2);
+    Matrix22 A33 = C.block(0, 0, 2, 2);
     return (A33.determinant() > 0);
   }
   return false;
@@ -271,7 +309,9 @@ bool DualConic::contains(const Point2& p) const {
 
   // apply a threshold due to noisy matrix inversion
   double thresh = 1e-10;
-  if (pointError <= thresh) { return true;}
+  if (pointError <= thresh) {
+    return true;
+  }
   return false;
 }
 
@@ -284,5 +324,5 @@ void DualConic::print(const string& s) const {
 bool DualConic::equals(const DualConic& other, double tol) const {
   return this->normalize().matrix().isApprox(other.normalize().matrix(), tol);
 }
-    
-} // namespace gtsam
+
+}  // namespace gtsam
