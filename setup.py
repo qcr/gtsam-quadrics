ce54749f16cc593c6f4dfe0dbf33ca125b94be0a
Ben Talbot <b.talbot@qut.edu.au>
2021-09-13 13:22:23 +1000

    Successfully build using setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,31 @@
+import os
 from setuptools import find_packages, setup, Extension
 from setuptools.command.build_ext import build_ext
 
 
 class CMakeExtension(Extension):
 
-    def __init__(self):
-        super().__init__('', sources=[])
+    def __init__(self, name, cmake_path):
+        self.cmake_path = cmake_path
+        super().__init__(name, sources=[])
 
 
 class cmake_build_ext(build_ext):
 
     def run(self):
+        for e in self.extensions:
+            self.build_cmake(e)
         super().run()
+
+    def build_cmake(self, ext):
+        print("BUILD DETAILS:")
+        source_dir = os.getcwd()
+        build_dir = self.build_temp
+
+        print(self.get_ext_fullpath(ext.name))
+
+        self.spawn(['cmake', '-B', build_dir, 'S', source_dir])
+        self.spawn(['cmake', '--build', build_dir])
 
 
 with open("README.md", 'r') as f:
@@ -27,7 +41,7 @@ setup(name='gtsam_quadrics',
       long_description_content_type='text/markdown',
       packages=find_packages(),
       install_requires=[],
-      ext_modules=[CMakeExtension('g2o_cpp', './g2o/cpp/CMakeLists.txt')],
+      ext_modules=[CMakeExtension('gtsam_quadrics', './CMakeLists.txt')],
       cmdclass={'build_ext': cmake_build_ext},
       classifiers=(
           "Development Status :: 4 - Beta",
