a34778640643b7e8e6f336d5a3ee506c15c1cca1
Ben Talbot <b.talbot@qut.edu.au>
2022-06-03 07:59:34 +1000

    Bump version number to v0.1.3
    Addresses #13.

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ with open("README.md", 'r') as f:
     long_description = f.read()
 
 setup(name='gtsam_quadrics',
-      version='0.1.2',
+      version='0.1.3',
       author='Lachlan Nicholson',
       author_email='lachlan.nicholson@hdr.qut.edu.au',
       maintainer='Ben Talbot',
